fa5b4b4031c477a0761fd4125b78102b43d3ac76
john meah <johnmeah@memreas.com>
2013-11-05 19:22:42 -0500

    5-NOV-2013 Fixed bugs with AddFriendToEvent - added check for group array

--- module/Application/src/Application/memreas/AddFriendtoevent.php
+++ module/Application/src/Application/memreas/AddFriendtoevent.php
@@ -18,8 +18,8 @@ class AddFriendtoevent {
     protected $AddNotification;
 
     public function __construct($message_data, $memreas_tables, $service_locator) {
-        
-        $this->message_data = $message_data;
+error_log("Enter AddFriendtoevent.__construct()" . PHP_EOL);
+    	$this->message_data = $message_data;
         $this->memreas_tables = $memreas_tables;
         $this->service_locator = $service_locator;
         $this->dbAdapter = $service_locator->get('doctrine.entitymanager.orm_default');
@@ -30,14 +30,13 @@ class AddFriendtoevent {
             $this->notification = new Notification($service_locator);
         }
         //$this->dbAdapter = $service_locator->get(MemreasConstants::MEMREASDB);
+error_log("Exit AddFriendtoevent.__construct()" . PHP_EOL);
     }
 
     public function exec() {
-
-
+error_log("Enter AddFriendtoevent.exec()" . PHP_EOL);
+error_log("Enter AddFriendtoevent.exec() xml ----> " . $_POST['xml'] . PHP_EOL);
         $data = simplexml_load_string($_POST['xml']);
-
-
         $friend_array = $data->addfriendtoevent->friends->friend;
         $user_id = (trim($data->addfriendtoevent->user_id));
         $event_id = (trim($data->addfriendtoevent->event_id));
@@ -48,165 +47,157 @@ class AddFriendtoevent {
         $time = time();
         $error = 0;
 
-        //add group to event_group
-        foreach ($group_array as $key => $value) {
-            $group_id = $value->group->group_id;
-            $check = "SELECT e  FROM  Application\Entity\EventGroup e  where e.event_id='" . $event_id . "' and e.group_id='" . $group_id . "'";
+		//add group to event_group
+		if (!empty($group_array)) {
+error_log("Enter AddFriendtoevent.exec() - !empty(group_array)". PHP_EOL);
+			foreach ($group_array as $key => $value) {
+				$group_id = $value->group->group_id;
+				if ($group_id != 'null') {
+					$check = "SELECT e  FROM  Application\Entity\EventGroup e  where e.event_id='" . $event_id . "' and e.group_id='" . $group_id . "'";
+					$statement = $this->dbAdapter->createQuery($check);
+					$result_check = $statement->getResult();
 
-            $statement = $this->dbAdapter->createQuery($check);
-            $result_check = $statement->getResult();
+					if ($result_check->count() > 0) {
+						$message1 = 'event group already exist.';
+						$status = 'Failure';
+					} else {
+						//insert
+						$tblEventGroup = new \Application\Entity\EventGroup();
+						$tblEventGroup->group_id = $group_id;
+						$tblEventGroup->event_id = $event_id;
 
+						$this->dbAdapter->persist($tblEventGroup);
+						$this->dbAdapter->flush();
 
+						$message1 = 'event group Successfully added ';
+						$status = 'Success';
+					}
+				} // end if ($group_id != 'null')
+			}// end foreach
+		} // end if (!empty($group_array)) 
 
-
-            if ($result_check->count() > 0) {
-                $message1 = 'event group already exist.';
-                $status = 'Failure';
-            } else {//insert
-                $tblEventGroup = new \Application\Entity\EventGroup();
-                $tblEventGroup->group_id = $group_id;
-                $tblEventGroup->event_id = $event_id;
-
-                $this->dbAdapter->persist($tblEventGroup);
-                $this->dbAdapter->flush();
-                $message1 = 'event group Successfully added ';
-                $status = 'Success';
-            }
-        }
-        
-        //get user name
-        
         //add friends to event loop
-        foreach ($friend_array as $key => $value) {
-            $network_name = addslashes(trim($value->network_name));
-            $friend_name = addslashes(trim($value->friend_name));
-            $profile_pic_url = stripslashes(trim($value->profile_pic_url));
-            $friend_query = "select f.friend_id ,f.network
-                    from Application\Entity\Friend f
-                    where f.network='$network_name' and f.social_username='$friend_name'";
-            $statement = $this->dbAdapter->createQuery($friend_query);
-            $result_friend = $statement->getOneOrNullResult();
-            // add to friend
-            if ($result_friend) {
-               
-                $friend_id = $result_friend['friend_id'];
-                $network_name = $result_friend['network'];
-            } else {
-                ////
-                $friend_id = UUID::getUUID($this->dbAdapter);
+		if (!empty($friend_array)) {
+	        foreach ($friend_array as $key => $value) {
+				$network_name = addslashes(trim($value->network_name));
+				$friend_name = addslashes(trim($value->friend_name));
+				$profile_pic_url = stripslashes(trim($value->profile_pic_url));
+				$friend_query = "select f.friend_id ,f.network from Application\Entity\Friend f where f.network='$network_name' and f.social_username='$friend_name'";
+				$statement = $this->dbAdapter->createQuery($friend_query);
+				$result_friend = $statement->getOneOrNullResult();
+				// add to friend
+				if ($result_friend) {
+					$friend_id = $result_friend['friend_id'];
+					$network_name = $result_friend['network'];
+				} 
+				else {
+	error_log("Enter AddFriendtoevent.exec() - insdide if (result_friend) else " . PHP_EOL);
+					////
+					$friend_id = UUID::getUUID($this->dbAdapter);
+					$tblFriend = new \Application\Entity\Friend();
+					$tblFriend->friend_id = $friend_id;
+					$tblFriend->network = $network_name;
+					$tblFriend->social_username = $friend_name;
+					$tblFriend->url_image = $profile_pic_url;
+					$tblFriend->create_date = $time;
+					$tblFriend->update_date = $time;
 
-                $tblFriend = new \Application\Entity\Friend();
-                $tblFriend->friend_id = $friend_id;
-                $tblFriend->network = $network_name;
-                $tblFriend->social_username = $friend_name;
-                $tblFriend->url_image = $profile_pic_url;
-                $tblFriend->create_date = $time;
-                $tblFriend->update_date = $time;
+					try {
+						$this->dbAdapter->persist($tblFriend);
+						$this->dbAdapter->flush();
+	error_log("Enter AddFriendtoevent.exec() - succeeded to insert tblFriend" . PHP_EOL);
+					} catch (\Exception $exc) {
+	error_log("Enter AddFriendtoevent.exec() - failure to insert tblFriend" . PHP_EOL);
+						$status = 'failure';
+						$error = 1;
+					}
+				} // end  if ($result_friend) else
 
+				// add to user_friend
+				if (isset($friend_id) && !empty($friend_id)) {
+					$check_user_frind = "SELECT u  FROM  Application\Entity\UserFriend u where u.user_id='$user_id' and u.friend_id='$friend_id'";
+				
+					$statement = $this->dbAdapter->createQuery($check_user_frind);
+					$r = $statement->getResult();
 
+					if (count($r) > 0) {
+						$status = "success";
+						$message .= $friend_name . " is already in your friend list. ";
+					} else {
 
-                try {
-                    $this->dbAdapter->persist($tblFriend);
-                    $this->dbAdapter->flush();
-                } catch (\Exception $exc) {
-                    $status = 'failure';
-                    $error = 1;
-                }
+						$tblUserFriend = new \Application\Entity\UserFriend();
+						$tblUserFriend->friend_id = $friend_id;
+						$tblUserFriend->user_id = $user_id;
 
-            }
-            // add to user_friend
-            if (isset($friend_id) && !empty($friend_id)) {
-                $check_user_frind = "SELECT u  FROM  Application\Entity\UserFriend u where u.user_id='$user_id' and u.friend_id='$friend_id'";
-                
-                $statement = $this->dbAdapter->createQuery($check_user_frind);
-                $r = $statement->getResult();
+						try {
+							$this->dbAdapter->persist($tblUserFriend);
+							$this->dbAdapter->flush();
+							
+							$message .= 'Event Friend Successfully added';
+							$status = 'Success';
+						} catch (\Exception $exc) {
+							$status = 'Failure';
+							$error = 1;
+						}
+					}
+				}
+				//adding friend to event
+				if (!empty($event_id) && $error == 0) {
+					$check_event_frind = "SELECT e FROM Application\Entity\EventFriend e  where e.event_id='$event_id' and e.friend_id='$friend_id'";
+					$statement = $this->dbAdapter->createQuery($check_event_frind);
+					$r = $statement->getResult();
 
-                if (count($r) > 0) {
-                    $status = "success";
-                    $message .= $friend_name . " is already in your friend list. ";
-                } else {
+					if (count($r) > 0) {
+						$status = "Success";
+						$message .= "$friend_name is already in your Event Friend list.";
+					} else {
+						//insert EventFriend
+						$tblEventFriend = new \Application\Entity\EventFriend();
+						$tblEventFriend->friend_id = $friend_id;
+						$tblEventFriend->event_id = $event_id;
 
-                    $tblUserFriend = new \Application\Entity\UserFriend();
-                    $tblUserFriend->friend_id = $friend_id;
-                    $tblUserFriend->user_id = $user_id;
+						try {
+							$this->dbAdapter->persist($tblEventFriend);
+							$this->dbAdapter->flush();
+							$message .= 'Event Friend Successfully added';
+							$status = 'Success';
+						} catch (\Exception $exc) {
+							$message.='';
+							$status = 'failure';
+						}
+					} // end if (count($r) > 0) else
 
+					//save nofication intable
+					$ndata['addNotification']['meta'] = $friend_name . ' want to add you to event';
+					if($network_name == 'memreas'){
+						$ndata = array('addNotification' => array(
+							'user_id' => $user_id,
+							'meta' => $friend_name . 'want to add you to event',
+							'notification_type' => \Application\Entity\Notification::ADD_FRIEND_TO_EVENT,
+							'links' => json_encode(array(
+										'event_id' => $event_id,
+										'from_id' => $user_id,
+									)),
+								)
+							);
+						$this->AddNotification->exec($ndata);
+						//send push message add user id
+						$this->notification->add($user_id);
+					} 
+					else {
+						$this->notification->addFriend($friend_id);
+					}
+				} // endif (!empty($event_id) && $error == 0)
+            } //end foreach
+        } // end if (!empty($friend_array))
 
-                    try {
-                        $this->dbAdapter->persist($tblUserFriend);
-                        $this->dbAdapter->flush();
-                            
-                        $message .= 'Event Friend Successfully added';
-                        $status = 'Success';
-                    } catch (\Exception $exc) {
-                        $status = 'Failure';
-                        $error = 1;
-                    }
-
-
-               
-                }
-            }
-            //adding friend to event
-            if (!empty($event_id) && $error == 0) {
-                $check_event_frind = "SELECT e FROM Application\Entity\EventFriend e  where e.event_id='$event_id' and e.friend_id='$friend_id'";
-                $statement = $this->dbAdapter->createQuery($check_event_frind);
-                $r = $statement->getResult();
-
-                if (count($r) > 0) {
-                    $status = "Success";
-                    $message .= "$friend_name is already in your Event Friend list.";
-                } else {
-                    //insert EventFriend
-                    $tblEventFriend = new \Application\Entity\EventFriend();
-                    $tblEventFriend->friend_id = $friend_id;
-                    $tblEventFriend->event_id = $event_id;
-
-
-                    try {
-                        $this->dbAdapter->persist($tblEventFriend);
-                        $this->dbAdapter->flush();
-                        $message .= 'Event Friend Successfully added';
-                        $status = 'Success';
-                    } catch (\Exception $exc) {
-                        $message.='';
-                        $status = 'failure';
-                    }
-                }
-
-                //save nofication intable
-                $ndata['addNotification']['meta'] = $friend_name . ' want to add you to event';
-                if($network_name == 'memreas'){
-                    $ndata = array('addNotification' => array(
-                        'user_id' => $user_id,
-                        'meta' => $friend_name . 'want to add you to event',
-                        'notification_type' => \Application\Entity\Notification::ADD_FRIEND_TO_EVENT,
-                        'links' => json_encode(array(
-                                    'event_id' => $event_id,
-                                    'from_id' => $user_id,
-                                )),
-                            )
-                        );
-                    $this->AddNotification->exec($ndata);
-                    //send push message add user id
-                    $this->notification->add($user_id);
-                    
-                }
-                else{
-                    $this->notification->addFriend($friend_id);
-                }
-                
-                
-                
-            }
-        }
-        
-        //set nofication data and call send method
-        
+        if (!empty($data['addNotification']['meta'])) {
+        	//set nofication data and call send method
             $this->notification->setMessage($ndata['addNotification']['meta']);
             $this->notification->type=\Application\Entity\Notification::ADD_FRIEND_TO_EVENT;
             $this->notification->event_id= $event_id;
-                
             $this->notification->send();
+        } //end if (!empty($data['addNotification']['meta']))
         
 
         //add friends to event loop end
@@ -220,8 +211,6 @@ class AddFriendtoevent {
         $xml_output.= "</addfriendtoeventresponse>";
         $xml_output.= "</xml>";
         echo $xml_output;
-    }
-
-}
-
+    } // end exec
+} //end class
 ?>
--- module/Application/src/Application/memreas/ListAllmedia.php
+++ module/Application/src/Application/memreas/ListAllmedia.php
@@ -15,24 +15,20 @@ class ListAllmedia {
     protected $dbAdapter;
 
     public function __construct($message_data, $memreas_tables, $service_locator) {
-error_log("ListAllmedia.__construct enter" . PHP_EOL);    
-        error_log("Inside__construct...");
+//error_log("ListAllmedia.__construct enter" . PHP_EOL);    
         $this->message_data = $message_data;
         $this->memreas_tables = $memreas_tables;
         $this->service_locator = $service_locator;
         $this->dbAdapter = $service_locator->get('doctrine.entitymanager.orm_default');
         //$this->dbAdapter = $service_locator->get(MemreasConstants::MEMREASDB);
-error_log("ListAllmedia.__construct exit" . PHP_EOL);    
+//error_log("ListAllmedia.__construct exit" . PHP_EOL);    
     }
 
     public function exec() {
-
-error_log("ListAllmedia.exec enter" . PHP_EOL);    
-error_log("ListAllmedia.exec xml ---> " . $_POST['xml'] . PHP_EOL);    
+//error_log("ListAllmedia.exec enter" . PHP_EOL);    
+//error_log("ListAllmedia.exec xml ---> " . $_POST['xml'] . PHP_EOL);    
         $data = simplexml_load_string($_POST['xml']);
-error_log("ListAllmedia.exec data ---> " . print_r($data, true) . PHP_EOL);    
-//echo "<pre>";
-//print_r($data);
+//error_log("ListAllmedia.exec data ---> " . print_r($data, true) . PHP_EOL);    
         $message = ' ';
         $containt = ' ';
         $user_id = trim($data->listallmedia->user_id);
@@ -57,25 +53,14 @@ error_log("ListAllmedia.exec data ---> " . print_r($data, true) . PHP_EOL);
         $xml_output.= "<listallmediaresponse>";
         $xml_output.= "<medias>";
 
-//$q = "select * from media where user_id='$user_id' and is_profile_pic='0' ORDER BY media.create_date DESC Limit $from ,$limit";
-// changed by milap to fetch the profile pic also - with girish - 11-7-2013
         $from = ($page - 1) * $limit;
         if (empty($event_id)) {
-error_log("ListAllmedia.exec if empty event_id ---> " . print_r($data, true) . PHP_EOL);    
             $q1 = "select m from Application\Entity\Media m where m.user_id='$user_id' ORDER BY m.create_date DESC";
             $statement = $this->dbAdapter->createQuery($q1);
             $statement->setMaxResults($limit);
             $statement->setFirstResult($from);
             $result = $statement->getArrayResult();
         } else {
-error_log("ListAllmedia.exec if empty event_id else  ---> " . print_r($data, true) . PHP_EOL);    
-error_log("ListAllmedia.exec event_id  ---> $event_id" . PHP_EOL);    
-            /* $q = "select media.metadata ,media.media_id
-              from Application\Entity\Media  as media
-              inner join Application\Entity\EventMedia as em on media.media_id=em.media_id
-              inner join Application\Entity\Event as event on em.event_id=event.event_id
-              where event.event_id='$event_id' ORDER BY media.create_date DESC Limit $from ,$limit"; */
-
             $qb = $this->dbAdapter->createQueryBuilder();
             $qb->select('media.media_id', 'media.metadata');
             $qb->from('Application\Entity\Media', 'media');
@@ -87,95 +72,85 @@ error_log("ListAllmedia.exec event_id  ---> $event_id" . PHP_EOL);
             $qb->setMaxResults($limit);
             $qb->setFirstResult($from);
 
-error_log("ListAllmedia.exec qb->getQuery()->getSQL()  ---> " . print_r($qb->getQuery()->getSQL(), true) . PHP_EOL);    
-
             $result = $qb->getQuery()->getArrayResult();
-error_log("ListAllmedia.exec result  ---> " . print_r($result, true) . PHP_EOL);    
-
         }
         
-            if (count($result) <= 0) {
-                $error_flage = 2;
-                $message = "No Record found for this Event";
-            } else {
-                $xml_output.="<page>$page</page>";
-                $xml_output.="<status>Success</status>";
-                $xml_output.="<message>Media List</message>";
-                foreach ($result as  $row ){
-                    $url79x80 = '';
-                    $url448x306 = '';
-                    $url98x78 = '';
-                    $thum_url = '';
-                    $is_download = 0;
+		if (count($result) <= 0) {
+			$error_flage = 2;
+			$message = "No Record found for this Event";
+		} 
+		else {
+			$xml_output.="<page>$page</page>";
+			$xml_output.="<status>Success</status>";
+			$xml_output.="<message>Media List</message>";
+			foreach ($result as  $row ){
+				$url79x80 = '';
+				$url448x306 = '';
+				$url98x78 = '';
+				$thum_url = '';
+				$is_download = 0;
 
 
-                    $json_array = json_decode($row['metadata'], true);
- error_log("json ----> ListAllmedia.exec metadata ---->" . $row['metadata'] . PHP_EOL);
-                    if (isset($json_array['type']['image']) && is_array($json_array['type']['image'])) {
-                        $type = "image";
-                        if (isset($json_array['S3_files']['79x80']))
-                            $url79x80 = $json_array['S3_files']['79x80'];
- error_log("json ----> ListAllmedia.exec url79x80 ---->" . $url79x80 . PHP_EOL);
- error_log("json ----> ListAllmedia.exec json_array['S3_files']['79x80'] ---->" . $json_array['S3_files']['79x80'] . PHP_EOL);
-                        if (isset($json_array['S3_files']['448x306']))
-                            $url448x306 = $json_array['S3_files']['448x306'];
- error_log("json ----> ListAllmedia.exec url448x306 ---->" . $url448x306 . PHP_EOL);
- error_log("json ----> ListAllmedia.exec json_array['S3_files']['448x306'] ---->" . $json_array['S3_files']['448x306'] . PHP_EOL);
-                        if (isset($json_array['S3_files']['98x78']))
-                            $url98x78 = $json_array['S3_files']['98x78'];
- error_log("json ----> ListAllmedia.exec url98x78 ---->" . $url98x78 . PHP_EOL);
- error_log("json ----> ListAllmedia.exec json_array['S3_files']['98x78'] ---->" . $json_array['S3_files']['98x78'] . PHP_EOL);
-                    }
-                    else if (isset($json_array['type']['video']) && is_array($json_array['type']['video'])) {
-                        $type = "video";
-                        $thum_url = isset($json_array['S3_files']['1080p_thumbails'][0]['Full']) ? $json_array['S3_files']['1080p_thumbails'][0]['Full'] : ''; //get video thum
-                        $url79x80 = isset($json_array['S3_files']['1080p_thumbails'][1]['79x80']) ? $json_array['S3_files']['1080p_thumbails'][1]['79x80'] : ''; //get video thum
-                        $url448x306 = isset($json_array['S3_files']['1080p_thumbails'][2]['448x306']) ? $json_array['S3_files']['1080p_thumbails'][2]['448x306'] : ''; //get video thum
-                        $url98x78 = isset($json_array['S3_files']['1080p_thumbails'][3]['98x78']) ? $json_array['S3_files']['1080p_thumbails'][3]['98x78'] : ''; //get video thum
-                    } 
-                    else if (isset($json_array['type']['audio']) && is_array($json_array['type']['audio'])) {
-                        $type = "audio";
-                        continue;
-                    }
-                    else
-                        $type = "Type not Mentioned";
+				$json_array = json_decode($row['metadata'], true);
+				if (isset($json_array['type']['image']) && is_array($json_array['type']['image'])) {
+					$type = "image";
+					if (isset($json_array['S3_files']['79x80']))
+						$url79x80 = $json_array['S3_files']['79x80'];
+					if (isset($json_array['S3_files']['448x306']))
+						$url448x306 = $json_array['S3_files']['448x306'];
+					if (isset($json_array['S3_files']['98x78']))
+						$url98x78 = $json_array['S3_files']['98x78'];
+				}
+				else if (isset($json_array['type']['video']) && is_array($json_array['type']['video'])) {
+					$type = "video";
+					$thum_url = isset($json_array['S3_files']['1080p_thumbails'][0]['Full']) ? $json_array['S3_files']['1080p_thumbails'][0]['Full'] : ''; //get video thum
+					$url79x80 = isset($json_array['S3_files']['1080p_thumbails'][1]['79x80']) ? $json_array['S3_files']['1080p_thumbails'][1]['79x80'] : ''; //get video thum
+					$url448x306 = isset($json_array['S3_files']['1080p_thumbails'][2]['448x306']) ? $json_array['S3_files']['1080p_thumbails'][2]['448x306'] : ''; //get video thum
+					$url98x78 = isset($json_array['S3_files']['1080p_thumbails'][3]['98x78']) ? $json_array['S3_files']['1080p_thumbails'][3]['98x78'] : ''; //get video thum
+				} 
+				else if (isset($json_array['type']['audio']) && is_array($json_array['type']['audio'])) {
+					$type = "audio";
+					continue;
+				}
+				else
+					$type = "Type not Mentioned";
 
-                    $url = isset($json_array['S3_files']['path']) ? $json_array['S3_files']['path'] : '';
-                    $media_name = basename($url);
-                    if (isset($json_array['local_filenames']['device'])) {
-                    	$device = (array) $json_array['local_filenames']['device'];
-                    } else {
-                    	$device = array();
-                    }
-                    if (in_array($user_id . '_' . $device_id, $device)) {
-                        $is_download = 1;
-                    }
+				$url = isset($json_array['S3_files']['path']) ? $json_array['S3_files']['path'] : '';
+				$media_name = basename($url);
+				if (isset($json_array['local_filenames']['device'])) {
+					$device = (array) $json_array['local_filenames']['device'];
+				} else {
+					$device = array();
+				}
+				if (in_array($user_id . '_' . $device_id, $device)) {
+					$is_download = 1;
+				}
 
-					$host = MemreasConstants::CLOUDFRONT_DOWNLOAD_HOST;
-					if (isset($data->listallmedia->RTMP)){
-						$host = MemreasConstants::CLOUDFRONT_STREAMING_HOST;
-					}
-					$xml_output.="<media>";
-					$xml_output.="<media_id>" . $row['media_id'] . "</media_id>";
-					$xml_output.="<main_media_url><![CDATA[" . $host . $url . "]]></main_media_url>";
-					$xml_output.="<is_downloaded>$is_download</is_downloaded>";
-					$xml_output.="<event_media_video_thum>";
-					$xml_output.=(!empty($thum_url)) ? MemreasConstants::CLOUDFRONT_DOWNLOAD_HOST . $thum_url : '';
-					$xml_output.= "</event_media_video_thum>";
-					$xml_output.="<media_url_79x80><![CDATA[";
-					$xml_output.=(!empty($url79x80)) ? MemreasConstants::CLOUDFRONT_DOWNLOAD_HOST . $url79x80 : '';
-					$xml_output.= "]]></media_url_79x80>";
-					$xml_output.="<media_url_98x78><![CDATA[";
-					$xml_output.=(!empty($url98x78)) ? MemreasConstants::CLOUDFRONT_DOWNLOAD_HOST . $url98x78 : '';
-					$xml_output.= "]]></media_url_98x78>";
-					$xml_output.="<media_url_448x306><![CDATA[";
-					$xml_output.=(!empty($url448x306)) ? MemreasConstants::CLOUDFRONT_DOWNLOAD_HOST . $url448x306 : '';
-					$xml_output.= "]]></media_url_448x306>";
-					$xml_output.="<type>$type</type>";
-					$xml_output.="<media_name><![CDATA[" . $media_name . "]]></media_name>";
-					$xml_output.="</media>";
-                }
-            }
+				$host = MemreasConstants::CLOUDFRONT_DOWNLOAD_HOST;
+				if (isset($data->listallmedia->RTMP)){
+					$host = MemreasConstants::CLOUDFRONT_STREAMING_HOST;
+				}
+				$xml_output.="<media>";
+				$xml_output.="<media_id>" . $row['media_id'] . "</media_id>";
+				$xml_output.="<main_media_url><![CDATA[" . $host . $url . "]]></main_media_url>";
+				$xml_output.="<is_downloaded>$is_download</is_downloaded>";
+				$xml_output.="<event_media_video_thum>";
+				$xml_output.=(!empty($thum_url)) ? MemreasConstants::CLOUDFRONT_DOWNLOAD_HOST . $thum_url : '';
+				$xml_output.= "</event_media_video_thum>";
+				$xml_output.="<media_url_79x80><![CDATA[";
+				$xml_output.=(!empty($url79x80)) ? MemreasConstants::CLOUDFRONT_DOWNLOAD_HOST . $url79x80 : '';
+				$xml_output.= "]]></media_url_79x80>";
+				$xml_output.="<media_url_98x78><![CDATA[";
+				$xml_output.=(!empty($url98x78)) ? MemreasConstants::CLOUDFRONT_DOWNLOAD_HOST . $url98x78 : '';
+				$xml_output.= "]]></media_url_98x78>";
+				$xml_output.="<media_url_448x306><![CDATA[";
+				$xml_output.=(!empty($url448x306)) ? MemreasConstants::CLOUDFRONT_DOWNLOAD_HOST . $url448x306 : '';
+				$xml_output.= "]]></media_url_448x306>";
+				$xml_output.="<type>$type</type>";
+				$xml_output.="<media_name><![CDATA[" . $media_name . "]]></media_name>";
+				$xml_output.="</media>";
+			}
+		}
         
         if ($error_flage) {
             $xml_output.="<status>Failure</status>";
@@ -196,7 +171,7 @@ error_log("ListAllmedia.exec result  ---> " . print_r($result, true) . PHP_EOL);
         $xml_output.="</listallmediaresponse>";
         $xml_output.="</xml>";
         echo $xml_output;
-error_log("ListAllmedia.exec xml_output ---> " . $xml_output . PHP_EOL);    
+//error_log("ListAllmedia.exec xml_output ---> " . $xml_output . PHP_EOL);    
     }
 
 }
