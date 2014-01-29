31a8d1d672b378ed9c7bbd9cb7f720d6e97c6b37
john meah <johnmeah@memreas.com>
2014-01-28 19:12:32 -0500

    28-JAN-2014 - still debugging ios registration

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -133,7 +133,6 @@ error_log("Inside indexAction" . PHP_EOL);
             $_POST['xml'] = $message_data['xml'];
             
         } else{
-error_log("Inside indexAction action = ".$_REQUEST['action'].PHP_EOL);
         	$actionname=  isset($_REQUEST['action'])?$_REQUEST['action']:''; 
             $codebase = "new_code";
             $message_data['xml']='';
