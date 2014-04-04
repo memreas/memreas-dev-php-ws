38b9ba3173a189a9691aa62decb4364a468f59dc
john meah <johnmeah@memreas.com>
2014-04-04 17:57:11 -0400

    4-APR-2014 updated constants for queue url

--- module/Application/src/Application/Model/MemreasConstants.php
+++ module/Application/src/Application/Model/MemreasConstants.php
@@ -11,10 +11,10 @@ class MemreasConstants {
 	const MEMREASDB = 'memreasintdb';
 	const S3BUCKET = "memreasdev";
 	const TOPICARN = "arn:aws:sns:us-east-1:004184890641:us-east-upload-transcode-worker-int";
-	const QUEUEURL = 'https://sqs.us-east-1.amazonaws.com/004184890641/awseb-e-8zdmyxfwy5-stack-AWSEBWorkerQueue-1RTF4RETMQXT8';
+	const QUEUEURL = 'https://sqs.us-east-1.amazonaws.com/004184890641/awseb-e-h3wmi62uit-stack-AWSEBWorkerQueue-F2GBB1163KXF';
 	// const ORIGINAL_URL = "http://memreas-dev-php-ws.localhost/";
-	const ORIGINAL_URL = "http://memreasdev-ws-elastic.elasticbeanstalk.com/";
-	const MEDIA_URL = "http://memreasdev-ws-elastic.elasticbeanstalk.com/?action=addmediaevent";
+	const ORIGINAL_URL = "http://memreasdev-ws-etc.elasticbeanstalk.com/";
+	const MEDIA_URL = "http://memreasdev-ws-etc.elasticbeanstalk.com/?action=addmediaevent";
 	const URL = "/index";
 	
 	// ElastiCache section
@@ -22,7 +22,6 @@ class MemreasConstants {
 	const ELASTICACHE_SERVER_PORT = "11211";
 	const ELASTICACHE_SERVER_USE = false;
 	
-	// const SITEURL = "http://memreasdev.elasticbeanstalk.com/eventapp_zend2.1";
 	const DATA_PATH = "/data/";
 	const MEDIA_PATH = "/media/";
 	const IMAGES_PATH = "/images/";
@@ -34,7 +33,5 @@ class MemreasConstants {
 	const AUDIO = "/data/media/upload_audio";
 	const CLOUDFRONT_STREAMING_HOST = 'rtmp://s1iq2cbtodqqky.cloudfront.net/cfx/st/mp4:';
 	const CLOUDFRONT_DOWNLOAD_HOST = 'http://d1ckv7o9k6o3x9.cloudfront.net/';
-	const MEMREAS_TRANSCODER_URL = 'http://memreasbackend.elasticbeanstalk.com/';
-	// const MEMREAS_TRANSCODER_URL = 'http://192.168.1.13/memreas-dev-php-backend/app/';
 	const MEMREAS_TRANSCODER_TOPIC_ARN = 'arn:aws:sns:us-east-1:004184890641:us-east-upload-transcode-worker-int';
 }
