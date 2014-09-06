103a87fc4fa25a276b384ec6f4ff888a4b8b4b4a
john meah <johnmeah@memreas.com>
2014-09-06 16:25:03 -0400

    updated bucket in constants to memreasdevsec

--- module/Application/src/Application/Model/MemreasConstants.bewQ.php
+++ module/Application/src/Application/Model/MemreasConstants.bewQ.php
@@ -6,26 +6,6 @@
 namespace Application\Model;
 
 class MemreasConstants {
-	//wsu url
-	const ORIGINAL_URL = "http://memreasdev-wsu.elasticbeanstalk.com/";
-	const MEDIA_URL = "http://memreasdev-wsu.elasticbeanstalk.com/?action=addmediaevent";
-	
-	//bew url
-	const QUEUEURL = 'https://sqs.us-east-1.amazonaws.com/004184890641/memreasdev-backend-worker';
-	const SIGNURLS = true;
-	
-	//memreasdev (5-SEP-2014 secured) related
-	const S3BUCKET = "memreasdevsec";
-	const CLOUDFRONT_STREAMING_HOST = 'rtmp://s1iq2cbtodqqky.cloudfront.net/cfx/st/mp4:';
-	const CLOUDFRONT_DOWNLOAD_HOST = 'http://d1ckv7o9k6o3x9.cloudfront.net/';
-<?php
-// ///////////////////////////////
-// Author: John Meah
-// Copyright memreas llc 2013
-// ///////////////////////////////
-namespace Application\Model;
-
-class MemreasConstants {
  	//wsg url
 	const ORIGINAL_URL = "http://memreasdev-wsg.elasticbeanstalk.com/";
 	const MEDIA_URL = "http://memreasdev-wsg.elasticbeanstalk.com/?action=addmediaevent";
@@ -73,38 +53,5 @@ class MemreasConstants {
 	const TW_OAUTH_TOKEN_SECRET = 't0wqWd0OpHrZTWYHvx9VqVl3iySDTfZklKkB6v1WaohxH';
 	const ADMIN_EMAIL ='admin@memreas.com';
 
-
-}const MEMREAS_PAY_URL = "http://memreasdev-pay.elasticbeanstalk.com";
-	
-	
-	//Same across...
-	const URL = "/index";
-	const MEMREAS_TRANSCODER = true;
-	const MEMREASDB = 'memreasintdb';
-	const S3HOST = 'https://s3.amazonaws.com/';
-	const EXPIRES = 36000; // 10 hour
-	
-	// ElastiCache section
-	const ELASTICACHE_SERVER_ENDPOINT = "memreasintcache.142tbh.cfg.use1.cache.amazonaws.com";
-	const ELASTICACHE_SERVER_PORT = "11211";
-	const ELASTICACHE_SERVER_USE = false;
-	const DATA_PATH = "/data/";
-	const MEDIA_PATH = "/media/";
-	const IMAGES_PATH = "/images/";
-	const USERIMAGE_PATH = "/media/userimage/";
-	const FOLDER_PATH = "/data/media/";
-	const FOLDER_AUDIO = "upload_audio";
-	const FOLDER_VIDEO = "uploadVideo";
-	const VIDEO = "/data/media/uploadVideo";
-	const AUDIO = "/data/media/upload_audio";
-	
-	const FB_APPID  = '462180953876554';
-	const FB_SECRET = '23dcd2db19b17f449f39bfe9e93176e6';
-	const FB_FBHREF = 'https://apps.facebook.com/462180953876554';
-	const TW_CONSUMER_KEY ='9jwg1vX4MgH7rfBzxqkcjI90f';
-	const TW_CONSUMER_SECRET = 'bDqOeHkJ7OIQ4QPNnT1PA9oz55gf51YW0REBo12aazGA0CBrbY';
-	const TW_OAUTH_TOKEN = '1941271416-UuUhh7XTVJ7npEjmgQHAypAnl0VmNqOKJ7BzMp2';
-	const TW_OAUTH_TOKEN_SECRET = 't0wqWd0OpHrZTWYHvx9VqVl3iySDTfZklKkB6v1WaohxH';
-	const ADMIN_EMAIL ='admin@memreas.com';
 
 }
