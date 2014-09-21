8a3c84d2c40c1da247813a46336f8a4a3390c903
john meah <johnmeah@memreas.com>
2014-09-21 16:53:27 -0400

    21-SEP-2014 testing url for twitter

--- module/Application/src/Application/memreas/AddFriendtoevent.php
+++ module/Application/src/Application/memreas/AddFriendtoevent.php
@@ -278,7 +278,7 @@ error_log("$friend_name is already in your Event Friend list. ---> $friend_id" .
                          * TODO: add href for !memreas 
                          */
                         //$nmessage = $userOBj->username . ' invites you to !' . $eventOBj->name;
-                        $nmessage = $userOBj->username . ' invites you to https://fe.memreas.com/ ---> !' . $eventOBj->name;
+                        $nmessage = $userOBj->username . ' invites you to '.'<a href="https://fe.memreas.com/index">!'.$eventOBj->name.'</a>';
                         // save nofication intable
                         $ndata = array(
                             'addNotification' => array(
