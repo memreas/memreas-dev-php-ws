e9bb411a235a1488bdd246ebbb9bf8db281bd63f
john meah <johnmeah@memreas.com>
2014-11-01 14:50:27 -0400

    1-NOV-2014 corrected db in global.php

---
+++ point2local.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Purpose = ssh or sftp into ec2 instance
+#Created on 4-APR-2014
+#Author = John Meah
+#Version 1.0
+set -v verbose #echo on
+
+cp module/Application/src/Application/Model/MemreasConstants.localhost.php module/Application/src/Application/Model/MemreasConstants.php
