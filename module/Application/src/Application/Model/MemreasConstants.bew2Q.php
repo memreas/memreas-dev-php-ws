6801cdfe2ee14085b6181ffb1dc0c5bd39dfaa99
john meah <johnmeah@memreas.com>
2014-12-01 08:57:40 -0500

    1-DEC-2014 update for wsg fix

--- module/Application/src/Application/Model/MemreasConstants.bew2Q.php
+++ module/Application/src/Application/Model/MemreasConstants.bew2Q.php
@@ -12,8 +12,8 @@ class MemreasConstants {
 	
 	//memreasdev2 urls
 	const WEB_URL = "https://fe2.memreas.com/";
-	const ORIGINAL_URL = "https://memreasdev-wsg.memreas.com/";
-	const MEDIA_URL = "https://memreasdev-wsg.memreas.com/?action=addmediaevent";
+	const ORIGINAL_URL = "https://memreasdev-ws2.memreas.com/";
+	const MEDIA_URL = "https://memreasdev-ws2.memreas.com/?action=addmediaevent";
 	const QUEUEURL = 'https://sqs.us-east-1.amazonaws.com/004184890641/memreasdev-bew2q';
 	const MEMREAS_PAY_URL = "https://memreasdev-pay.memreas.com";
 	
