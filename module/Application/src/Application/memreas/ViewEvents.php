3d7d5b323cdcd2077d46bf5eda87ded9de9c5bae
kamlesh-pawar <kamleshpawar2000@yahoo.com>
2014-12-04 11:37:03 +0530

    fixing view event

--- module/Application/src/Application/memreas/ViewEvents.php
+++ module/Application/src/Application/memreas/ViewEvents.php
@@ -58,14 +58,13 @@ error_log("View Events.xml_input ---->  " . $_POST ['xml'] . PHP_EOL);
                 from Application\Entity\Event e
                 where e.user_id='" . $user_id . "'
                     and  (e.viewable_to >=" . $date . " or e.viewable_to ='')
-                       and  (e.viewable_from <=" . $date . " or e.viewable_from ='')
                     and  (e.self_destruct >=" . $date . " or e.self_destruct='')
                 ORDER BY e.create_time DESC";
             $statement = $this->dbAdapter->createQuery($query_event);
             $statement->setMaxResults($limit);
             $statement->setFirstResult($from);
             $result_event = $statement->getResult();
-
+l
             if ($result_event) {
                 if (count($result_event) <= 0) {
                     $xml_output .= "<status>Failure</status>";
@@ -400,6 +399,7 @@ error_log("viewevents friends xml output----> ".$xml_output.PHP_EOL);
                         and event.user_id=user.user_id
                         and event.user_id != '$user_id'
                         and  (event.viewable_to >=" . $date . " or event.viewable_to ='')
+                        and  (event.viewable_from <=" . $date . " or event.viewable_from ='')
                         and  (event.self_destruct >=" . $date . " or event.self_destruct='')
                         ORDER BY event.create_time DESC ";
 //error_log("Inside Public event dql string ----> " . $q_public . PHP_EOL);
