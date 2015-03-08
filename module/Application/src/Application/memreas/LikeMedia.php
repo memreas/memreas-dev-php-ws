3febf895bf463a01f6a2c864e351fb80d3a33969
john meah <johnmeah@memreas.com>
2015-03-07 22:46:31 -0500

    .

--- module/Application/src/Application/memreas/LikeMedia.php
+++ module/Application/src/Application/memreas/LikeMedia.php
@@ -21,85 +21,108 @@ class LikeMedia {
 	}
 	public function exec() {
 		$data = simplexml_load_string ( $_POST ['xml'] );
-		// echo "<pre>";
-		// print_r($data);
 		$message = ' ';
+		$event_id = trim ( $data->likemedia->event_id );
 		$media_id = trim ( $data->likemedia->media_id );
 		$user_id = trim ( $data->likemedia->user_id );
 		$is_like = trim ( $data->likemedia->is_like );
 		$time = time ();
-		if (! isset ( $media_id ) || empty ( $media_id )) {
-			$message = 'media_id is empty';
+		$isIdSet = 0;
+		
+		if (isset ( $media_id ) && ! empty ( $media_id )) {
+			$isIdSet = 1;
+		} else {
+			$media_id = "";
+		}
+		
+		if (isset ( $event_id ) && ! empty ( $event_id )) {
+			$isIdSet = 1;
+		} else {
+			$event_id = "";
+		}
+		
+		$status = 'ok';
+		if (! $isIdSet) {
+			$message = 'event_id and media_id are empty';
 			$status = 'Failure';
-		} else if (! isset ( $is_like ) || empty ( $is_like )) {
+		}
+		if (! isset ( $is_like ) || empty ( $is_like )) {
 			$message = 'is_like is empty';
 			$status = 'Failure';
-		} else if (! isset ( $user_id ) || empty ( $user_id )) {
+		}
+		if (! isset ( $user_id ) || empty ( $user_id )) {
 			$message = 'user_id is empty';
 			$status = 'Failure';
-		} else {
-			$q = "select c.comment_id from Application\Entity\Comment c where c.media_id ='$media_id' and c.type = 'like' and c. user_id='$user_id' AND c.like=1";
-			// $result = mysql_query($q);
-			// $statement = $this->dbAdapter->createStatement($q);
-			// $result = $statement->execute();
-			// $row = $result->current();
-
+		}
+		
+		if ($status != 'Failure') {
+			$q = "select c.comment_id from Application\Entity\Comment c" . " where (c.media_id ='$media_id' or c.event_id='$event_id')" . " and c.type = 'like' " . " and c. user_id='$user_id'" . " AND c.like=1";
+			
 			$statement = $this->dbAdapter->createQuery ( $q );
 			$row = $statement->getResult ();
-
+			
 			if (! empty ( $row [0] )) {
 				$status = 'Success';
 				$message = 'You Already Like This Media';
 			} else {
-				$q_event_media = "SELECT m  FROM Application\Entity\EventMedia m where m.media_id='$media_id'";
-				// $result_event_media = mysql_query($q_event_media);
-				// $statement = $this->dbAdapter->createStatement($q_event_media);
-				// $result = $statement->execute();
-				// $result_event_media = $result->current();
-				$statement = $this->dbAdapter->createQuery ( $q_event_media );
-				$result_event_media = $statement->getResult ();
-				// echo '<pre>';print_r($result_event_media);exit;
-
-				if (empty ( $result_event_media [0] )) {
-
-					$status = 'Failure';
-					$message = "No Media for this Event";
-				} else {
+				/*
+				 * Check media and/or event id
+				 */
+				if (!empty($media_id)) {
+					$q_event_media = "SELECT m  FROM Application\Entity\EventMedia m where m.media_id='$media_id'";
+					
+					$statement = $this->dbAdapter->createQuery ( $q_event_media );
+					$result_event_media = $statement->getResult ();
+					
+					if (empty ( $result_event_media [0] )) {
+						
+						$status = 'Failure';
+						$message = "No Media for this Event";
+					}
+				} else if (!empty($event_id)) {
+					$q_event = "SELECT e  FROM Application\Entity\Event e where e.event_id='$event_id'";
+					
+					$statement = $this->dbAdapter->createQuery ( $q_event );
+					$result_event = $statement->getResult ();
+					
+					if (empty ( $result_event [0] )) {
+						
+						$status = 'Failure';
+						$message = "No Event found";
+					}
+				}
+				/*
+				 * If ok insert...
+				 */
+				if ($status != 'Failure') {
 					$comment_id = MUUID::fetchUUID ();
-					// $row = mysql_fetch_assoc($result_event_media);
-
+					if(empty($event_id)) {
+						$event_id = $result_event_media [0]->event_id;
+					}
+					
 					$tblComment = new \Application\Entity\Comment ();
 					$tblComment->comment_id = $comment_id;
 					$tblComment->media_id = $media_id;
 					$tblComment->user_id = $user_id;
 					$tblComment->type = 'like';
-                    $tblComment->like = 1;
-					$tblComment->event_id = $result_event_media [0]->event_id;
+					$tblComment->like = 1;
+					$tblComment->event_id = $event_id;
 					$tblComment->create_time = $time;
 					$tblComment->update_time = $time;
-
+					
 					$this->dbAdapter->persist ( $tblComment );
 					$this->dbAdapter->flush ();
-
-					/*
-					 * $q_comment = "INSERT INTO Application\Entity\Comment (comment_id,`media_id`, `user_id`, `type`, `event_id`, `create_time`, `update_time`) VALUES ('$comment_id','$media_id', '$user_id', 'like', '" . $row['event_id'] . "', '$time', '$time')";
-					 */
-					// mysql_query($q_comment) or die(mysql_error());
-					// $statement = $this->dbAdapter->createStatement($q_comment);
-					// $result = $statement->execute
-					// $statement = $this->dbAdapter->createQuery($q_comment);
-					// $result = $statement->getResult();
-
+					
 					$status = "Success";
 					$message .= "You Like succesfully";
 				}
 			}
 		}
-
+		
 		header ( "Content-type: text/xml" );
 		$xml_output = "<?xml version=\"1.0\"  encoding=\"utf-8\" ?>";
 		$xml_output .= "<xml>";
-
+		
 		$xml_output .= "<likemediaresponse>";
 		$xml_output .= "<status>" . $status . "</status>";
 		$xml_output .= "<message>" . $message . "</message>";
