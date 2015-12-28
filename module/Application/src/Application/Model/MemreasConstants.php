3555f0466223000480ebe588d3cacf42569459f4
john meah <johnmeah@memreas.com>
2015-12-28 11:37:26 -0500

    .syntax error in memreas constants

--- module/Application/src/Application/Model/MemreasConstants.php
+++ module/Application/src/Application/Model/MemreasConstants.php
@@ -1,5 +1,6 @@
 <?php
-//**
+
+/**
  * Copyright (C) 2015 memreas llc. - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
