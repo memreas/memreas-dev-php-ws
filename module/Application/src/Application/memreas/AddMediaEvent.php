e1b6a7c346584f928d33742657f4bc6e16f2f18d
john meah <johnmeah@memreas.com>
2015-12-30 21:43:20 -0500

    .

--- module/Application/src/Application/memreas/AddMediaEvent.php
+++ module/Application/src/Application/memreas/AddMediaEvent.php
@@ -132,7 +132,7 @@ class AddMediaEvent {
 					//
 					// Update media to set all rows to 0 for this user's profile pic
 					//
-					$q = $this->em->createQueryBuilder ()->update ( 'Application\Entity\Media', 'm' )->set ( 'm.is_profile_pic', 0 )->where ( 'm.user_id = ?1' )->setParameter ( 1, $user_id )->getQuery ();
+					$q = $this->dbAdapter->createQueryBuilder ()->update ( 'Application\Entity\Media', 'm' )->set ( 'm.is_profile_pic', 0 )->where ( 'm.user_id = ?1' )->setParameter ( 1, $user_id )->getQuery ();
 					$p = $q->execute ();
 				}
 				//
