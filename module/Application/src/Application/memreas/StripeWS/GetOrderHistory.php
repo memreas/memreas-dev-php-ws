a91f5eef32ba3cff5b77feb7e1c9125201bb568d
kamlesh <kamleshpawar2000@yahoo.com>
2016-03-01 09:41:03 +0530

    ..

--- module/Application/src/Application/memreas/StripeWS/GetOrderHistory.php
+++ module/Application/src/Application/memreas/StripeWS/GetOrderHistory.php
@@ -61,7 +61,7 @@ class GetOrderHistory {
 		]);
 		
  		$data = json_decode ( $response->getBody (), true );
-		
+		error_log('resultws ----->'.print_r($data));
 		$status = $data ['status'];
 		
 		if ($status == 'Success') {
