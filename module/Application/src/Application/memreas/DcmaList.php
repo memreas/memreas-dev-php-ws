1171ff5db9657c57af7562022cd7947dde41b928
kamlesh <kamleshpawar2000@yahoo.com>
2016-03-02 11:34:40 +0530

    ..

--- module/Application/src/Application/memreas/DcmaList.php
+++ module/Application/src/Application/memreas/DcmaList.php
@@ -53,7 +53,7 @@ class DcmaList {
              
              
             $qb = $this->dbAdapter->createQueryBuilder();
-            $qb->select('dcma,m.metadata  media_meta');
+            $qb->select('dcma.*' ,'m.metadata  media_meta');
             $qb->from('Application\Entity\DcmaViolation', 'dcma');
             $qb->join('Application\Entity\Media', 'm', 'WITH', 'm.media_id = dcma.media_id');
             $qb->join('Application\Entity\User', 'u', 'WITH', 'u.user_id = dcma.user_id');
@@ -61,7 +61,7 @@ class DcmaList {
             $qb->orderBy('dcma.create_time', 'DESC');
             $qb->setParameter(1, $user_id);
              
-            $result = $qb->getQuery()->getResult();
+            $result = $qb->getQuery()->getArrayResult();
             $dcmalist='';
             if(empty($result )){
                 $status = 'Failure';
