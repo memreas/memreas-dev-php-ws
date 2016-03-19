aa9613d36d536ec8aa1e3867c5cf3f6a96006455
john meah <johnmeah@memreas.com>
2016-03-19 15:59:07 -0400

    .update for redis ip

--- module/Application/src/Application/Model/MemreasConstants.php
+++ module/Application/src/Application/Model/MemreasConstants.php
@@ -26,7 +26,7 @@ class MemreasConstants {
 	const MEMREAS_PAY_URL_INDEX = "https://memreasprod-pay.memreas.com/index/";
 	
 	// Redis section
-	const REDIS_SERVER_ENDPOINT = "10.178.192.105";
+	const REDIS_SERVER_ENDPOINT = "10.154.58.83";
 	const REDIS_SERVER_USE = true;
 	const REDIS_SERVER_SESSION_ONLY = true;
 	const REDIS_SERVER_PORT = "6379";
