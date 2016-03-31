6aea59a67f6ec60019c00d1e4cf217a64367f897
john meah <johnmeah@memreas.com>
2016-03-31 17:56:33 -0400

    updated poin2server.sh

---
+++ point2server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Purpose = ssh or sftp into ec2 instance
+#Created on MAR-2016
+#Author = John Meah
+#Version 1.0
+set -v verbose #echo on
+
+cp module/Application/src/Application/Model/MemreasConstants.bewQ.php module/Application/src/Application/Model/MemreasConstants.php
