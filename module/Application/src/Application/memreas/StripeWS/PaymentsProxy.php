dab7e9e43083e6f04f1ef79faaa6c7bd892121cc
john meah <johnmeah@memreas.com>
2016-03-31 12:37:56 -0400

    debugging subscription call get disk usage

--- module/Application/src/Application/memreas/StripeWS/PaymentsProxy.php
+++ module/Application/src/Application/memreas/StripeWS/PaymentsProxy.php
@@ -54,6 +54,7 @@ class PaymentsProxy {
 		 */
 		if ($action_method == 'subscribe') {
 			$disk_usage = new GetDiskUsage ( $this->service_locator );
+			Mlog::addone($cm.__LINE__.'::paymentsproxy->$_SESSION', $_SESSION);
 			$jsonArr ['disk_usage'] = $disk_usage->exec ( $_SESSION ['user_id'], true );
 		}
 		/*
