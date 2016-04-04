97278e6d8701771f4cea510b43febb8a2641b9ec
john meah <johnmeah@memreas.com>
2016-04-04 09:55:29 -0400

    change for caching to only use for sessions

--- module/Application/src/Application/Model/MemreasConstants.php
+++ module/Application/src/Application/Model/MemreasConstants.php
@@ -28,7 +28,7 @@ class MemreasConstants {
 	// Redis section
 	const REDIS_SERVER_ENDPOINT = "10.154.58.83";
 	const REDIS_SERVER_USE = true;
-	const REDIS_SERVER_SESSION_ONLY = true;
+	const REDIS_SERVER_SESSION_ONLY = false;
 	const REDIS_SERVER_PORT = "6379";
 	const REDIS_CACHE_TTL = 3600;
 	// 1 hour
