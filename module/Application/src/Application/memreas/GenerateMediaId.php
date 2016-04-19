7e2775bfd985899f4a59485c02d483b30d8965ab
john meah <johnmeah@memreas.com>
2016-04-19 17:33:28 -0400

    changed case of generatemediaid for xml

--- module/Application/src/Application/memreas/GenerateMediaId.php
+++ module/Application/src/Application/memreas/GenerateMediaId.php
@@ -15,9 +15,9 @@ class GenerateMediaId {
 		$cm = __CLASS__ . __METHOD__;
 		$data = simplexml_load_string ( $_POST ['xml'] );
 		Mlog::addone ( $cm . '::$data---->', $data, 'p' );
-		Mlog::addone ( $cm . '::$data->media_id_batch---->', $data->media_id_batch );
+		Mlog::addone ( $cm . '::$data->generatemediaid->media_id_batch---->', $data->media_id_batch );
 		
-		if ($data->generateMediaId->media_id_batch) {
+		if ($data->generatemediaid->media_id_batch) {
 			$media_id_batch = array();
 			for($i=0;$i<=MemreasConstants::media_id_batch_create_count; $i++) {
 				$media_id_batch[] = MUUID::fetchUUID ();
