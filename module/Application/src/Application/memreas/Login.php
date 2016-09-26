17dc54680ec94dc17ac770fe571570cf2e81e2d5
john meah <johnmeah@memreas.com>
2016-09-26 08:44:28 -0400

    fixed syntax error

--- module/Application/src/Application/memreas/Login.php
+++ module/Application/src/Application/memreas/Login.php
@@ -5,7 +5,7 @@
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  */
-namespace Application\mm;
+namespace Application\memreas;
 
 
 use Application\Model\MemreasConstants;
